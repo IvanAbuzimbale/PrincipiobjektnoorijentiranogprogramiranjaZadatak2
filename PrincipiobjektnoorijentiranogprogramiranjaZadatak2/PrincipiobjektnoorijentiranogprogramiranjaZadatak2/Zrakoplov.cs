21fb3731908baa6d4a90c8588e277b03edaa341c
IvanAbuzimbale <58472137+IvanAbuzimbale@users.noreply.github.com>
2021-10-18 09:14:50 +0200

    Add files via upload

---
+++ PrincipiobjektnoorijentiranogprogramiranjaZadatak2/PrincipiobjektnoorijentiranogprogramiranjaZadatak2/Zrakoplov.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrincipiobjektnoorijentiranogprogramiranjaZadatak2
+{
+    class Zrakoplov
+    {
+        private string Naziv;
+        private double SnagaMotora;
+        private int DosegLeta;
+        public Zrakoplov(string naziv, double snaga, int doseg)
+        {
+            Naziv = naziv;
+            SnagaMotora = snaga;
+            DosegLeta = doseg;
+        }
+    }
+}
